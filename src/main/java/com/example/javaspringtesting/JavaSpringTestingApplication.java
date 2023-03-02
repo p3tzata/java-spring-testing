26d195b9574825c173803b20833153991d18e228
Petar Chavdarov <petar.chavdarov@rewe-digital.com>
2023-03-02 08:40:22 +0200

    first commit

---
+++ src/main/java/com/example/javaspringtesting/JavaSpringTestingApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaspringtesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javaspringtesting/JavaSpringTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaspringtesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
